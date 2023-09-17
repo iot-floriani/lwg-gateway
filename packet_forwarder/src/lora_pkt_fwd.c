58494c9ab9afd41abf6ede61fa4dc41607530440
IoT - Floriani <36849973+iot-floriani@users.noreply.github.com>
2023-09-17 16:42:13 +0200

    Update lora_pkt_fwd.c

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -2484,8 +2484,8 @@ void thread_up(void) {
 
         /* send datagram to server */
         send(sock_up, (void *)buff_up, buff_index, 0);
-		/* send datagram to server url*/
-		requests(serv_url,(void *)buff_up);		
+	/* send datagram to server url*/
+	/* requests(serv_url,(void *)buff_up);*/		
         clock_gettime(CLOCK_MONOTONIC, &send_time);
         pthread_mutex_lock(&mx_meas_up);
         meas_up_dgram_sent += 1;
