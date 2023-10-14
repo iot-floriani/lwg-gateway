bb200f769447752fb2255c541131c7f570080b71
IoT - Floriani <36849973+iot-floriani@users.noreply.github.com>
2023-10-14 22:15:51 +0200

    Add files via upload

--- packet_forwarder/src/lora_pkt_fwd.c
+++ packet_forwarder/src/lora_pkt_fwd.c
@@ -2436,7 +2436,7 @@ void thread_up(void) {
                 exit(EXIT_FAILURE);
             }
 
-            w = snprintf((char *)(buff_wh + buff_wh_index), TX_BUFF_SIZE-buff_wh_index, "\",\"size\":%u},\"gateway_ids\":{\"eui\":\"%016llX\",\"temp\":%.1f}",p->size,lgwm,stat_temperature);
+            w = snprintf((char *)(buff_wh + buff_wh_index), TX_BUFF_SIZE-buff_wh_index, "\",\"size\":%u},\"gateway_ids\":{\"eui\":\"%016llX\",\"rssi\":%.0f,\"snr\":%.1f}",p->size,lgwm,roundf(p->rssic),p->snr);
             if (w > 0) {
                 buff_wh_index += w;
             } else {
